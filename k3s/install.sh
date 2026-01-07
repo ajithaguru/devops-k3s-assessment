7f29f01738b109594aa81150c8d146e908394637
ajithaguru <ajithaguru7@gmail.com>
2026-01-07 15:51:38 +0530

    Update install.sh
    k3s setup

--- k3s/install.sh
+++ k3s/install.sh
@@ -1,1 +1,11 @@
 # k3s/install.sh
+Install k3s (single node cluster)
+sudo apt update -y sudo apt install -y curl curl -sfL https://get.k3s.io | sh - 
+sudo systemctl status k3s --no-pager
+#setting up kubectl
+mkdir -p $HOME/.kube 
+sudo cp /etc/rancher/k3s/k3s.yaml 
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+#verify kubectl
+kubectl get nodes 
+kubectl get pods -A
